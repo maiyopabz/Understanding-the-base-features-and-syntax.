9ff2e9788fcc01834f4904e862d855dd1b4f1fc8
Maiyo P <maiyopabz@gmail.com>
2020-08-23 01:25:42 -0700

    Creating a Function Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div ClassName= 'App'>
+
+        <h1> Hi, Im react App</h1>
+        <p> yooooooo</p>
+        <Person />
+
+      </div>
+    )
+  }
 }
 
 export default App;
